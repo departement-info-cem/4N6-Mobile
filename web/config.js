c1b820f1723f355f213997b8365eb5dda3501a10
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2023-10-24 12:52:11 -0400

    Migration repo

--- web/config.js
+++ web/config.js
@@ -1,7 +1,7 @@
 const config = {
     nom: "4N6 - Applications Mobiles",
     description: "Site de référence du cours d'applications mobiles",
-    nomUrl: "4N6-Mobile-Nouveau"
+    nomUrl: "4N6-Mobile"
 }
 
 module.exports = config;
