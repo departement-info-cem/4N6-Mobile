01290c9dff48ca54f0662d38b4245fde42f25183
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2024-04-01 12:09:37 -0400

    Format string des erreurs

--- code/HTTPResponse/HTTPResponse_400_Description/src/main/java/org/kickmyb/server/RESTExceptionHandler.java
+++ code/HTTPResponse/HTTPResponse_400_Description/src/main/java/org/kickmyb/server/RESTExceptionHandler.java
@@ -2,23 +2,26 @@ package org.kickmyb.server;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.nio.charset.StandardCharsets;
+
 // Va intercepter toutes les exceptions et les transformer selon la méthode en réponse http
 
 @ControllerAdvice
-public class RESTExceptionHandler
-  extends ResponseEntityExceptionHandler {
+public class RESTExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = { Exception.class })
+    @ExceptionHandler(value = {Exception.class})
     protected ResponseEntity<Object> handleConflict(Exception ex, WebRequest request) {
         ex.printStackTrace();
         String bodyOfResponse = ex.getClass().getSimpleName();
-        return handleExceptionInternal(ex, bodyOfResponse, 
-          new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("text", "plain", StandardCharsets.UTF_8));
+        return handleExceptionInternal(ex, bodyOfResponse, headers, HttpStatus.BAD_REQUEST, request);
     }
 }
