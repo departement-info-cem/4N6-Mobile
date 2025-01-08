b9df0e747273d899217f6200300158fcf74f9dbb
Joris Deguet <joris.deguet@gmail.com>
2025-01-08 16:40:00 -0500

    Fin TestCookie pour les cookie + post

--- code/Retrofit/05-Cookies/app/src/androidTest/java/org/depinfo/retrofit_demo/TestCookie.kt
+++ code/Retrofit/05-Cookies/app/src/androidTest/java/org/depinfo/retrofit_demo/TestCookie.kt
@@ -1,16 +1,17 @@
 package org.depinfo.retrofit_demo
 
-import android.util.Log
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.depinfo.retrofit_demo.http.UtilitaireRetrofit
 import org.depinfo.retrofit_demo.transfer.SignupRequest
 import org.junit.Test
 import org.junit.runner.RunWith
 import retrofit2.Call
-import java.io.IOException
 
 @RunWith(AndroidJUnit4::class)
 class TestCookie {
+
+    // ouvrir https://kickmyb-server.onrender.com/ dans un navigateur
+    // pour réveiller le serveur
 
     // Passe une fois mais pas 2
     // Peut ne pas passer parce que le serveur n'est pas parti
@@ -19,12 +20,12 @@ class TestCookie {
         // service > retrofit > client okhttp > cookie Jar
         val service = UtilitaireRetrofit.get()
         val request = SignupRequest()
-        request.username = "joris@pipo.org"
+        request.username = "raoul@pipo.org"
         request.password = "123456"
         val call: Call<String> = service.signup(request)
         val response = call.execute()
         val resultat = response.body()
-        assert(resultat!!.contains("joris@pipo.org"))
+        assert(resultat!!.contains("raoul@pipo.org"))
     }
 
     // Lancer ce test seulement
@@ -32,15 +33,17 @@ class TestCookie {
     fun testSignupPuisHome() {
         val service = UtilitaireRetrofit.get()
         val request = SignupRequest()
-        request.username = "joris@pipo.org"
+        var random = (0..1000).random()
+        request.username = "joris"+random.toString()+"@pipo.org"
         request.password = "123456"
         val call: Call<String> = service.signup(request)
         val response = call.execute()
         val resultat = response.body()
-
+        println("Retour du signup : " + resultat)
         val callHome: Call<String> = service.home()
         val responseHome = callHome.execute()
         val resultatHome = responseHome.body()
+        println("Retour du home : " + resultatHome)
         assert(resultatHome != null)
     }
 
