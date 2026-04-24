d53f4da34abdd9d30e725db23c1eb9ebdfbf3aae
Benoit Caldairou <benoit.caldairou@cegepmontpetit.ca>
2026-04-24 13:26:27 -0400

    [11.1] Correction d'erreurs de compilation

--- code/ErreurRetrofitCompose/app/build.gradle.kts
+++ code/ErreurRetrofitCompose/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -37,6 +38,9 @@ android {
     buildFeatures {
         compose = true
     }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }
 
 dependencies {
@@ -53,6 +57,7 @@ dependencies {
     implementation(libs.retrofit.converter.gson)
     implementation(libs.okhttp)
     implementation(libs.okhttp.logging.interceptor)
+    implementation(libs.core.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
